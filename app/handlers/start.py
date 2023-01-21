6d40eca3794cfa84c251c056aceeae0388eb0da0
volkrav-iMac <volodymyr.kravchenko@icloud.com>
2023-01-21 15:47:32 +0200

    some fix

--- app/handlers/start.py
+++ app/handlers/start.py
@@ -11,14 +11,13 @@ logger = logging.getLogger(__name__)
 
 
 async def command_start(message: types.Message, state: FSMContext):
+    logger.info(
+        f'{message.from_user.id} started work')
     current_state = await state.get_state()
-    answer = ''
     if not current_state:
-        logger.info(
-            f'{message.from_user.id} started work')
         answer = (
-            f'- Привітальна інформація\n'
-            f'- Короткий опис, як працює чекер.\n'
+            'Бот для перевірки наявності світла.\n' +
+            'Побудований на основі системи моніторингу мережевих пристроїв за допомогою ping.'
         )
         await message.answer(answer)
     answer = 'Будь ласка, оберіть потрібний розділ ⤵️'
@@ -28,6 +27,7 @@ async def command_start(message: types.Message, state: FSMContext):
         reply_markup=reply.kb_start
     )
 
+
 async def get_photo_id(message: types.Message):
     await message.reply(message.photo[-1].file_id)
 
@@ -36,4 +36,5 @@ def register_start(dp: Dispatcher):
     dp.register_message_handler(command_start,
                                 CommandStart(),
                                 state='*')
-    dp.register_message_handler(get_photo_id, content_types=types.ContentType.PHOTO, state='*')
+    dp.register_message_handler(
+        get_photo_id, content_types=types.ContentType.PHOTO, state='*')
